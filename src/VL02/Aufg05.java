f1f9752f4a82555cd5e873bc06ba6616a0b2c806
Sora <Sora.E-C@gmx.de>
2021-10-11 19:16:16 +0200

    Update Aufg05

--- src/VL02/Aufg05.java
+++ src/VL02/Aufg05.java
@@ -1,4 +1,18 @@
 package VL02;
 
+import java.util.Scanner;
+
 public class Aufg05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Im Folgenden wird irgendeine Rechnung durchgeführt. Surpriiiiise!");
+        System.out.print("Geben Sie bitte ");
+        System.out.println("eine beliebige ganze Zahl ein");
+        int input = sc.nextInt();
+        final int x = input;
+        int mon1 = 5 * x;
+        int mon2 = mon1 + 3 * x * x;
+        mon1 = mon2 + x * x * x;
+        System.out.println(mon1);
+    }
 }
